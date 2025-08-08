c86609b2abf8615a76753d6147ccf7dde11b3641
OliverGT22 <109478445+OliverGT22@users.noreply.github.com>
2025-08-07 22:44:21 -0600

    correcciones de entorno para firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {
   provideRouter,
@@ -13,6 +13,14 @@ import { DropdownModule, SidebarModule } from '@coreui/angular';
 import { IconSetService } from '@coreui/icons-angular';
 import { routes } from './app.routes';
 
+// Importar Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+import { provideServiceWorker } from '@angular/service-worker';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes,
@@ -25,10 +33,16 @@ export const appConfig: ApplicationConfig = {
       }),
       withEnabledBlockingInitialNavigation(),
       withViewTransitions(),
-      withHashLocation()
+      withHashLocation(),
     ),
     importProvidersFrom(SidebarModule, DropdownModule),
     IconSetService,
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firestoreConfig)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()), provideServiceWorker('ngsw-worker.js', {
+              enabled: !isDevMode(),
+              registrationStrategy: 'registerWhenStable:30000'
+            })
   ]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firestoreConfig:{
+        apiKey: "AIzaSyC7AT1NjElMYQIjHNLdkzyCWdkdLT2FRfE",
+        authDomain: "cumplimiento-marcos-legales.firebaseapp.com",
+        projectId: "cumplimiento-marcos-legales",
+        storageBucket: "cumplimiento-marcos-legales.firebasestorage.app",
+        messagingSenderId: "38508255332",
+        appId: "1:38508255332:web:7a6ffab45e4188018ce771",
+        measurementId: "G-6JPKGB35H6"
+    }
+};
