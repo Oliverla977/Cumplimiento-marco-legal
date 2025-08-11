fc13613f6c4c22c68be7a55ef6bb3b097751b2e6
OliverGT22 <109478445+OliverGT22@users.noreply.github.com>
2025-08-10 18:10:41 -0600

    se agrega variable global para url de api

--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -2,29 +2,30 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from "rxjs";
 import { UsuarioModel } from "../model/usuario.model";
 import { HttpClient } from '@angular/common/http';
-
+import { URL_API } from '../global/vars';
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-  
-  private apiUrl = 'http://localhost:3000/usuarios';
+
+  // Use the global variable for the API URL
+  private apiUrl : string = URL_API;
 
   constructor(private http: HttpClient) { }
 
   registrarUsuario(usuario: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/registro`, usuario);
+    return this.http.post(`${this.apiUrl}/usuarios/registro`, usuario);
   }
 
   obtenerUsuarios(): Observable<{ success: boolean; data: UsuarioModel[] }> {
-    return this.http.get<{ success: boolean; data: UsuarioModel[] }>(this.apiUrl);
+    return this.http.get<{ success: boolean; data: UsuarioModel[] }>(`${this.apiUrl}/usuarios/`);
   }
 
   deshabilitarUsuario(id_usuario: number): Observable<any> {
-  return this.http.put(`${this.apiUrl}/deshabilitar/${id_usuario}`, {});
+  return this.http.put(`${this.apiUrl}/usuarios/deshabilitar/${id_usuario}`, {});
 }
 
   habilitarUsuario(id_usuario: number): Observable<any> {
-    return this.http.put(`${this.apiUrl}/habilitar/${id_usuario}`, {});
+    return this.http.put(`${this.apiUrl}/usuarios/habilitar/${id_usuario}`, {});
   }
 }
