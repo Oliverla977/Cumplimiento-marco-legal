ce430b8f006cd0051c79c3493231ceb83cb45723
OliverGT22 <109478445+OliverGT22@users.noreply.github.com>
2025-08-23 01:39:33 -0600

    deploy segmentacion de roles y carga de json

--- src/app/global/vars.ts
+++ src/app/global/vars.ts
@@ -1,3 +1,3 @@
-export const URL_API = 'http://localhost:3000';
-//export const URL_API = 'https://api-marco-legal.onrender.com';
+//export const URL_API = 'http://localhost:3000';
+export const URL_API = 'https://api-marco-legal.onrender.com';
 //export const URL_API = 'http://proyectomarco-001-site1.ntempurl.com/api';
