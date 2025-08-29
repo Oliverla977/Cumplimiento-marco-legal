e9c941349315df8f0bff39c6aa6258e1f768c1dc
OliverGT22 <109478445+OliverGT22@users.noreply.github.com>
2025-08-28 23:02:56 -0600

    deploy con api publica

--- src/app/global/vars.ts
+++ src/app/global/vars.ts
@@ -1,2 +1,2 @@
-export const URL_API = 'http://localhost:3000';
-//export const URL_API = 'https://api-marco-legal.onrender.com';
+//export const URL_API = 'http://localhost:3000';
+export const URL_API = 'https://api-marco-legal.onrender.com';
